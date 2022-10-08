e3d087bb6cd8c71e8716f655a3b713bf0b922f6b
Mark Kaladzinsky <kristianomark7@gmail.com>
2022-10-08 16:22:22 +0300

    init task 3

---
+++ Task3.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Test;
+
+class Task3
+{
+    public function main(int $inputNumber): int
+    {
+        if ($inputNumber < 10) {
+            return $inputNumber;
+        }
+
+        $arrOfNumbers = str_split((string)$inputNumber);
+
+        while (count($arrOfNumbers) != 1) {
+            $sum = 0;
+            foreach ($arrOfNumbers as $arrOfNumber) {
+                $sum += (int)$arrOfNumber;
+            }
+            $arrOfNumbers = str_split((string)$sum);
+        }
+        return $sum;
+    }
+}
