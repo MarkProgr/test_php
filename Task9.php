bcef0c73fa84707c1223fc923bfc75b58c28560f
Mark Kaladzinsky <kristianomark7@gmail.com>
2022-10-10 11:13:09 +0300

    init task 9

---
+++ Task9.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Solutions;
+
+class Task9
+{
+    public function main(array $array, int $number)
+    {
+        $count = count($array) - 2;
+        $newArr = [];
+        for ($i = 0; $i < $count; $i++) {
+            if (($array[$i] + $array[$i+1] + $array[$i+2]) === $number) {
+                $newArr[] = "{$array[$i]} + {$array[$i+1]} + {$array[$i+2]} = {$number}";
+            }
+        }
+        return $newArr;
+    }
+}
